ef3d09acf9442ba9c4a9d48905dab8f3a28d8e71
vamsi-31 <105601899+vamsi-31@users.noreply.github.com>
2025-03-20 16:19:42 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/health-management', {
+mongoose.connect(process.env.MONGO_URI', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
